bae543d3f2e3b9e127a127d970233190282288d5
gjee22 <99296995+gjee22@users.noreply.github.com>
2024-08-22 15:58:15 +0900

    [Bronze I] Title: N번째 큰 수, Time: 132 ms, Memory: 15764 KB -BaekjoonHub

--- 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.java
+++ 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.java
@@ -3,23 +3,26 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 import java.util.Arrays;
+import java.lang.StringBuilder;
 
 class Main {
-	private static int N = 10;
 	private static int K = 7;
+	private static int N = 10;
+	private static int[] arr = new int[N];
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder res = new StringBuilder();
 		int numCases = Integer.parseInt(st.nextToken()); 
 		for (int i = 0; i < numCases; i++) {
 			st = new StringTokenizer(br.readLine());
-			int[] arr = new int[N];
 			for (int j = 0; j < N; j++) {
 				arr[j] = Integer.parseInt(st.nextToken());
 			}
 			Arrays.sort(arr);
-			System.out.println(arr[K]);
+			res.append(arr[K]).append("\n");
 		}
+		System.out.print(res.toString());
 	}
 }
