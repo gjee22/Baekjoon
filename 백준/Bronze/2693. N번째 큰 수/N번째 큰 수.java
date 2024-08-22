f7761c9fcd7f3b8316c14a4223ef81ea4a6d6a9f
gjee22 <99296995+gjee22@users.noreply.github.com>
2024-08-22 15:46:51 +0900

    [Bronze I] Title: N번째 큰 수, Time: 156 ms, Memory: 16108 KB -BaekjoonHub

--- 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.java
+++ 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.Arrays;
 
 class Main {
 	private static int N = 10;
@@ -17,42 +18,8 @@ class Main {
 			for (int j = 0; j < N; j++) {
 				arr[j] = Integer.parseInt(st.nextToken());
 			}
-			findThirdLargest(arr);
+			Arrays.sort(arr);
+			System.out.println(arr[K]);
 		}
-	}
-	
-	private static void findThirdLargest(int[] arr) {
-		int start = 0;
-		int end = N;
-		int pivotLoc = start;
-
-		while (pivotLoc != K) {
-		  pivotLoc = start;
-			int pivot = arr[pivotLoc];
-			int i = start;
-			int j = end - 1;
-			
-			while (i < j) {
-			  while (i < j && arr[j] > pivot) j--;
-				while (i < j && arr[i] <= pivot) i++;
-				int temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
-			}
-			
-			int temp = arr[i];
-			arr[i] = arr[pivotLoc];
-			arr[pivotLoc] = temp;
-			pivotLoc = i;
-			
-			if (pivotLoc < K) {
-				start = pivotLoc + 1;
-				end = end;
-			} else {
-				start = start;
-				end = pivotLoc;
-			}
-		}
-		System.out.println(arr[pivotLoc]);
 	}
 }
