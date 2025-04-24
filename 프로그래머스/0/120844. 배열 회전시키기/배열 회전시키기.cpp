54a92a37dc206b982155380269e6371faed6cee7
gjee22 <99296995+gjee22@users.noreply.github.com>
2025-04-24 19:02:30 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.cpp
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.cpp
@@ -1,20 +1,14 @@
 #include <string>
 #include <vector>
-#include <deque>
+#include <algorithm>
 
 using namespace std;
 
 vector<int> solution(vector<int> numbers, string direction) {
-    deque<int> nums;
-    for (int i = 1; i < size(numbers) - 1; i++) {
-        nums.emplace_back(numbers[i]);
-    }
-    if (direction.compare("right") == 0) {
-        nums.emplace_front(numbers[0]);
-        nums.emplace_front(numbers[size(numbers) - 1]);
+    if (direction.compare("right")) {
+        rotate(numbers.begin(), numbers.begin() + 1, numbers.end());
     } else {
-        nums.emplace_back(numbers[size(numbers) - 1]);
-        nums.emplace_back(numbers[0]);
+        rotate(numbers.begin(), numbers.end() - 1, numbers.end());
     }
-    return vector<int>(nums.begin(), nums.end());
+    return numbers;
 }
