72a8db7c9f2203e032fff338ae6acb4bfb74addd
gjee22 <99296995+gjee22@users.noreply.github.com>
2025-04-25 13:21:16 +0900

    [level 1] Title: 폰켓몬, Time: 0.65 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -1,16 +1,10 @@
 #include <vector>
-#include <unordered_map>
+#include <unordered_set>
 using namespace std;
 
 int solution(vector<int> nums)
 {
     int answer = 0;
-    unordered_map<int, int> map;
-    for (auto i : nums) {
-        if (map.find(i) == map.end()) {
-            map.emplace(i, 0);
-        }
-        map[i]++;
-    }
-    return min(size(map), size(nums) / 2);
+    unordered_set<int> set (nums.begin(), nums.end());
+    return min(size(set), size(nums) / 2);
 }
